6bcdd6cbe0d56e0de9d58433e73caf3fa84a65bd
Enrique-Pesqueda <enrique7831@gmail.com>
2020-06-26 16:33:24 -0700

    scroll to sections implemented

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -9,4 +9,17 @@ export class AppComponent {
   title = 'portfolio';
   
 
+  toAbout(){
+    document.getElementById("about-me").scrollIntoView({behavior:"smooth"});
+  }
+  toExperience(){
+    document.getElementById("jobs").scrollIntoView({behavior:"smooth"});
+  }
+  toProjects(){
+    document.getElementById("projects").scrollIntoView({behavior:"smooth"});
+  }
+  toContact(){
+    document.getElementById("get-in-contact").scrollIntoView({behavior:"smooth"});
+  }
+
 }
